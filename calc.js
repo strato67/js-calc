44ccac13f4d5419f47811891e067aa5409a9e924
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-05-04 20:36:14 -0400

    new

--- calc.js
+++ calc.js
@@ -1,1 +1,18 @@
-const joe = ""
+const add = (a,b)=>parseFloat(a)+parseFloat(b);
+const subtract = (a,b)=>parseFloat(a)-parseFloat(b);
+const multiply = (a,b)=>parseFloat(a)*parseFloat(b);
+const divide = (a,b)=>parseFloat(a)/parseFloat(b);
+
+
+const operate = (a,b,op) =>{
+
+    const operations = {
+        '+': add(a,b),
+        '-': subtract(a,b),
+        '*': multiply(a,b),
+        '/': divide(a,b)
+    }
+    return operations[op];
+
+
+}
