9134b1409ab907fed6c8ffcc5d15e3c985f8eff1
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-05-08 15:03:11 -0400

    button

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,37 +1,42 @@
 const clearBtn = document.querySelector('.clear');
 const display = document.querySelector('.display');
 const buttons = document.querySelector('.container').querySelectorAll('div');
+var calcQueue = "";
 
-const clearDisplay = () =>display.innerText = "";
+const clearDisplay = () =>{
+    display.innerText = "";
+    calcQueue = "";
+}
+
+const negativeHelper = (queue) =>{
+    for(let i = 0; i<queue.length; i++){
+        if(queue[i]=='-'&& queue.length>3){
+            queue[i]+=queue[i+1]
+            queue.splice(i+1,1)
+        }
+
+    }
+    return queue;
+}
 
 const calculateAnswer = () =>{
-    tempQueue = calcQueue.split(/(?=[*/+-])|(?<=[*/+-])/g);
-    if(tempQueue.length == 3){
+    calcQueue = display.innerText;
+    let tempQueue = negativeHelper(calcQueue.split(/(?=[*/+-])|(?<=[*/+-])/g));
+    if(tempQueue.length >= 3){
+
+        
         display.innerText = operate(tempQueue[0],tempQueue[2],tempQueue[1]);
     }  
 
 }
 
-var calcQueue = "";
-
 buttons.forEach((button)=>{
     if(button.className=='clear'){
-        button.addEventListener('click', () => {
-            clearDisplay()
-            calcQueue = "";
-          });
-    
+        button.addEventListener('click',clearDisplay);
     }
     else if(button.className=='equal'){
-        button.addEventListener('click', () => {
-            calcQueue = display.innerText;
-            
-            calculateAnswer();
-            
-          });
-    
+        button.addEventListener('click',calculateAnswer);
     }
-    
     else{
         
         button.addEventListener('click', () => {
@@ -39,6 +44,5 @@ buttons.forEach((button)=>{
             
         });
     }
-
 });
 
